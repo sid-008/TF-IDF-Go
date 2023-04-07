c2477e40c412c7785e50f305545a7359010a5601
Siddharth Tewari <siddharthtewari14@gmail.com>
2023-04-07 18:50:38 +0530

    Added: idf and tfidf computation

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"regexp"
 	"strings"
 )
@@ -13,12 +14,12 @@ type Document struct {
 }
 
 func NewDoc(content string) *Document {
-	re, err := regexp.Compile(`[^\w]`)
+	re, err := regexp.Compile(`[^\w]`) //clean the string remove punctuations. There should be a better way to do this tbh
 	if err != nil {
 		log.Fatal(err)
 	}
 	content = re.ReplaceAllString(content, " ")
-	doc := &Document{
+	doc := &Document{ //doc is defined
 		Content: content,
 		Terms:   make(map[string]int),
 	}
@@ -29,9 +30,36 @@ func NewDoc(content string) *Document {
 	return doc
 }
 
+func ComputeIDF(docs []*Document, term string) float64 {
+	n := float64(len(docs))
+	df := 0.0
+	for _, doc := range docs {
+		if doc.Terms[term] > 0 {
+			df++
+		}
+	}
+	idf := math.Log(n / df)
+	return idf
+}
+
+func ComputeTFIDF(doc *Document, docs []*Document, term string) float64 {
+	tf := float64(doc.Terms[term]) / float64(len(doc.Terms))
+	idf := ComputeIDF(docs, term)
+	tfidf := tf * idf
+	return tfidf
+}
+
 func main() {
-	doc1 := `Hello from something something, I do something something, there is something in the way hmmmm mmmmm something in the way`
-	test := NewDoc(doc1)
-	fmt.Println(test.Terms)
+	docs := []*Document{
+		NewDoc("This is a sample document."),
+		NewDoc("Another sample document."),
+		NewDoc("And yet another sample document."),
+	}
+	for _, doc := range docs {
+		for term := range doc.Terms {
+			tfidf := ComputeTFIDF(doc, docs, term)
+			fmt.Printf("%s %f\n", term, tfidf)
+		}
+	}
 
 }
